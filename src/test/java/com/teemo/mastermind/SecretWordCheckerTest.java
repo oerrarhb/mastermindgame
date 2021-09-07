87e81aa0490194f9fdfe9baab40e6240bec0e13a
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2021-09-07 16:15:17 +0200

    Init testing

--- src/test/java/com/teemo/mastermind/SecretWordCheckerTest.java
+++ src/test/java/com/teemo/mastermind/SecretWordCheckerTest.java
@@ -1,7 +1,15 @@
 package com.teemo.mastermind;
 
-public class SecretWordCheckerTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class SecretWordCheckerTest {
+    @Test
+    public void testCutList() {
+        assertEquals("", SecretWordChecker.check("1234","5555"));
+
+    }
 
 
 
