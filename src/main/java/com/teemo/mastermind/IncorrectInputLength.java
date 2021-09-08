6ff5bb073052154bbf5e8fa93f60047c2b07ad39
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2021-09-08 22:20:45 +0200

    Add Error Handling

---
+++ src/main/java/com/teemo/mastermind/IncorrectInputLength.java
@@ -0,0 +1,7 @@
+package com.teemo.mastermind;
+
+public class IncorrectInputLength extends Exception {
+    public IncorrectInputLength(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- src/main/java/com/teemo/mastermind/SecretWordChecker.java
+++ src/main/java/com/teemo/mastermind/SecretWordChecker.java
@@ -4,8 +4,11 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class SecretWordChecker {
+
+    private static final Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
     public static String checkSimilarity(String userEntry, String secretCode) {
         if (userEntry.equals(secretCode)) {
@@ -37,26 +40,53 @@ public class SecretWordChecker {
         return outPut;
     }
 
+
     public static void main(String[] argv) {
         var args = new Args();
         JCommander.newBuilder()
                 .addObject(args)
                 .build()
                 .parse(argv);
-
-        var scanner = new Scanner(System.in);
-        while (!Thread.currentThread().isInterrupted()) {
-            System.out.println("Try guess secret word ?");
-            var userEntry = scanner.nextLine();
-            var resultOfCheck = checkSimilarity(userEntry, args.secretCode);
-            System.out.println(resultOfCheck);
-            if (resultOfCheck.equals("++++")) {
-                System.out.println("You Won ! ");
-                scanner.close();
-                Thread.currentThread().interrupt();
+        try (var scanner = new Scanner(System.in)) {
+            var secretCode = args.secretCode;
+            if (secretCode.isEmpty()) {
+                throw new IncorrectInputLength("Secret Code must not be empty !");
             }
+            if (!pattern.matcher(secretCode).matches()) {
+                throw new NumberFormatException("Secret Code must contain only numbers !");
+            }
+            if (secretCode.length() != 4) {
+                throw new IncorrectInputLength("Secret Code Must contain 4 numbers !");
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    System.out.println("Try guess secret word ?");
+                    var userEntry = scanner.nextLine();
+                    if (userEntry.isEmpty()) {
+                        throw new IncorrectInputLength("Try not Empty Entry !");
+                    }
+                    if (!pattern.matcher(userEntry).matches()) {
+                        throw new NumberFormatException("Input Should Contain Only Numbers !");
+                    }
+                    if (userEntry.length() != 4) {
+                        throw new IncorrectInputLength("Try entry with 4 numbers !");
+                    }
+                    var resultOfCheck = checkSimilarity(userEntry, secretCode);
+                    System.out.println(resultOfCheck);
+                    if (resultOfCheck.equals("++++")) {
+                        System.out.println("You Won ! ");
+                        scanner.close();
+                        Thread.currentThread().interrupt();
+                    }
+                } catch (IncorrectInputLength | NumberFormatException userEntryError) {
+                    userEntryError.printStackTrace();
+                }
+            }
+        } catch (IncorrectInputLength | NumberFormatException userEntryError) {
+            userEntryError.printStackTrace();
         }
     }
+
 
     static class Args {
         @Parameter(names = "--secret-word")
